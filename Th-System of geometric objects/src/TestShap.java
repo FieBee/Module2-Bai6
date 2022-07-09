9482a6ffbbf2b321a625b54870df61ef3929c07a
FieBee <phuonglf98@gmail.com>
2022-07-09 14:40:42 +0700

    add Th System of  geometric objects

---
+++ Th-System of geometric objects/src/TestShap.java
@@ -0,0 +1,10 @@
+public class TestShap {
+        public static void main(String[] args) {
+            Shap shap = new Shap();
+            System.out.println(shap);
+
+            Shap shappp = new Shap("red",false);
+            System.out.println(shappp);
+        }
+
+}
